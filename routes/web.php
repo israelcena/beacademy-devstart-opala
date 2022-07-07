a287ef9399d1c06c177708d110e14fedd41a10d5
Israel Cena <israelsjm@gmail.com>
2022-07-07 17:14:15 -0300

    criação da rota para exibir todos os usuários

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('users.index');
+Route::controller(UserController::class)->group(function () {
+    Route::get('/', 'index')->name('users.index');
 });
