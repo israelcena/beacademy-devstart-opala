f06aa24a06d706036b6e6d68b15e6d9f3bde6321
Adriano de Oliveira <adriano.98@gmail.com>
2022-07-09 22:15:49 -0300

    Instalando dependências para autenticação do projeto / breeze

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(UserController::class)->group(function () {
-    Route::get('/', 'index')->name('users.index');
-    Route::post('/novousuario/criar', 'store')->name('users.store');
-    Route::get('/novousuario', 'create')->name('users.create');
-    Route::delete('/usuario/{id}', 'destroy')->name('users.destroy');
-    Route::get('/usuarios/{id}', 'showOne')->name('users.showOne');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
