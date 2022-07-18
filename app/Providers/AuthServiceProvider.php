c1f0273b29420954dbe8c2ddd6f04c3996210173
Adriano de Oliveira <adriano.98@gmail.com>
2022-07-18 19:43:34 -0300

    Adicionando Policy User

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('user-show', function (User $loggedUser, User $user) {
+            return $loggedUser->is($user);
+        });
     }
 }
