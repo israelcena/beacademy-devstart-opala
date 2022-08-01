8fa9cc25295cb10c176fac217d466de152530b60
Daniel Attias <danielattias@live.com>
2022-08-01 17:48:01 -0400

    Edição de Estilos Home e Admin

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,9 @@
     <div>
         @include('layouts.header')
     </div>
+    <div class="section-hero">
+        <h1 class="text-center" style="line-height:center">Escolha os doces que desejar!</h1>
+    </div>
     <div class="container">
         @if(Session::has('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -11,34 +14,10 @@
         @endif
     </div>
 
-    <div class="container mt-5">
-        <div class="row d-flex">
-            <div class="col-md-12 mt-5">
-                <h1 class="text-center">Escolha os doces que desejar:</h1>
-                <div class="row">
-                    @foreach($products as $product)
-                        <div class="col-md-3 mb-3">
-                            <div class="card -lg position-relative d-flex border-none" style="height: 38rem;">
-                                <img src="{{ asset('storage/' . $product->image_products) }}" class="card-img-top img-fluid mx-auto mt-1" alt="{{ $product->name }}" style="max-width: 120px">
-                                    <div class="card-body d-flex flex-column">
-                                        <h5 class="card-title" style="font-size: 1.8rem;">{{ $product->name }}</h5>
-                                        <p class="card-text mt-auto">{{ $product->description }}</p>
-                                        <p class="text-muted mt-auto">Valor: R$ {{ $product->salesPrice }}</p>
-                                    </div>
-                                    <div class="card-footer bg-transparent mb-2">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                                <a href="{{ route('products.showOne', $product->id) }}" class="btn btn-sm btn-outline-secondary">Ver detalhes</a>
-                                                <a href="{{ route('cart.add', ['id' => $product->id]) }}" class="btn btn-sm btn-outline-secondary">Adicionar ao carrinho</a>
-                                        </div>
-
-                                    </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>                           
-        </div>
+    <div class="container mt-5 show-produtos">
+        @include('layouts.products')
     </div>
+
     <div>
         @include('layouts.footer')
     </div>
